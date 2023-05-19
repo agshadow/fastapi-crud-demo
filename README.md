0d38916323576787454fff19e55b29b4fa17bfb9
agshadow <91526956+agshadow@users.noreply.github.com>
2023-05-19 21:38:47 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Fast api tutorial
+https://www.youtube.com/watch?v=gV-EpY2TeQ0
+
+
+# how to run it
+
+uvicorn main:app --debug
---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+import pathlib
+from typing import List, Union
+
+from fastapi import FastAPI, Response
+
+from models import Track
+
+app = FastAPI()
+
+data = []
+
+@app.on_event("startup")
+async def startup_event():
+    datapath = pathlib.Path() / 'data' / 'tracks.json'
+    with open (datapath, 'r') as f:
+        tracks = json.load(f)
+        for track in tracks:
+            data.append(Track(**track).dict())
+    
+@app.get('/tracks/', response_model=List[Track])
+def tracks():
+    return data
+
+@app.get('/tracks/{track_id}', response_model=Union[Track,str])
+def tracks(track_id: int, response: Response):
+    track = None
+    for t in data:
+        if t['id'] == track_id:
+            track = t
+            break
+    if track is None:
+        response.status_code = 404
+        return "Track not found"
+    return track
