ae16d8d86ab8236a141b9a8ac3ed486ad2d959e8
agshadow <agshadow@gmail.com>
2023-05-20 18:23:34 +1000

    finished tutorial

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import json
 import pathlib
 from typing import List, Union
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, Depends
+
+
+from sqlmodel import Session, select
+
+from database import TrackModel, engine, create_tables, delete_database
 
 from models import Track
 
@@ -10,34 +15,98 @@ app = FastAPI()
 
 data = []
 
+
 @app.on_event("startup")
 async def startup_event():
-    datapath = pathlib.Path() / 'data' / 'tracks.json'
-    with open (datapath, 'r') as f:
-        tracks = json.load(f)
-        for track in tracks:
-            data.append(Track(**track).dict())
-    
-@app.get('/tracks/', response_model=List[Track])
-def tracks():
-    return data
+    datapath = pathlib.Path() / "data" / "tracks.json"
 
-@app.get('/tracks/{track_id}', response_model=Union[Track,str])
-def tracks(track_id: int, response: Response):
-    track = None
-    for t in data:
-        if t['id'] == track_id:
-            track = t
-            break
+    result = None
+    with Session(engine) as session:
+        stmt = select(TrackModel)
+        result = session.exec(stmt).first()
+
+    if result is None:
+        print("loading json")
+
+        with Session(engine) as session1:
+            with open(datapath, "r") as f:
+                tracks = json.load(f)
+                for track in tracks:
+                    session.add(TrackModel(**track))
+            session.commit()
+    else:
+        print("data already loded")
+
+
+# get_session dependency
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.get("/tracks/", response_model=List[Track])
+def tracks(session: Session = Depends(get_session)):
+    result = None
+    stmt = select(TrackModel)
+    result = session.exec(stmt).all()
+    print("returning result")
+    return result
+
+
+@app.get("/tracks/{track_id}", response_model=Union[Track, str])
+def tracks(track_id: int, response: Response, session: Session = Depends(get_session)):
+    result = None
+    stmt = select(TrackModel).where(TrackModel.id == track_id)
+    track = session.exec(stmt).first()
+    print("returning result", track)
     if track is None:
         response.status_code = 404
         return "Track not found"
     return track
 
-@app.post('/tracks/', response_model=Track, status_code = 201)
-def create_track(track:Track):
-    track_dict = track.dict()
-    track_dict['id'] = max(data, key=lambda x: x['id']).get('id') + 1
-    data.append(track_dict)
-    return track_dict
-                           
+
+@app.post("/tracks/", response_model=Track, status_code=201)
+def create_track(track: TrackModel, session: Session = Depends(get_session)):
+    session.add(track)
+    session.commit()
+    session.refresh(track)
+    return track
+
+
+@app.put("/tracks/{track_id}", response_model=Union[Track, str])
+def tracks(
+    track_id: int,
+    updated_track: TrackModel,
+    response: Response,
+    session: Session = Depends(get_session),
+):
+    track = None
+    track = session.exec(select(TrackModel).where(TrackModel.id == track_id)).first()
+
+    if track is None:
+        response.status_code = 404
+        return "Track not found"
+    track_dict = updated_track.dict(exclude_unset=True)
+    for key, val in track_dict.items():
+        setattr(track, key, val)
+    session.add(track)
+    session.commit()
+    session.refresh(track)
+    return track
+
+
+@app.delete("/tracks/{track_id}")
+def tracks(
+    track_id: int,
+    response: Response,
+    session: Session = Depends(get_session),
+):
+    track = session.get(TrackModel, track_id)
+    if track is None:
+        response.status_code = 404
+        print("track not founddddd")
+        return "Track not found"
+
+    session.delete(track)
+    session.commit()
+    return Response(status_code=200)
--- powershellscripts/request.ps1
+++ powershellscripts/request.ps1
@@ -1,10 +1,39 @@
-$Uri = 'http://127.0.0.1:8000/tracks/1'
-$Form = ${
-    artist= "Sonic Youth"
-    title= "Silver Rocket"
-    last_play= "2017-10-18 15:15:26"
-    duration= 200
-}
+
+working scripts:
+1:
+
+Invoke-WebRequest -Method 'Post' -Uri http://127.0.0.1:8000/tracks/ -Body '{"artist": "Sonic Youth", "title": "Silver Rocket", "last_play": "2017-10-18 15:15:26", "duration": 200}' -ContentType "application/json"
+
+2:
+$Url = 'http://127.0.0.1:8000/tracks/'
+$Form = '{"artist": "Sonic Youth", "title": "Silver Rocket", "last_play": "2017-10-18 15:15:26", "duration": 200}'
+Invoke-WebRequest -Method 'Post' -Uri $Url -Body $Form -ContentType "application/json"
+
+
+3:
+$Url = 'http://127.0.0.1:8000/tracks/1'
+$Form = '{"artist": "Sonic Youth", "title": "Silver Rocket", "last_play": "2017-10-18 15:15:26", "duration": 200}'
+Invoke-WebRequest -Method 'PUT' -Uri $Url -Body $Form -ContentType "application/json"
+
+
+3:
+$Url = 'http://127.0.0.1:8000/tracks/1'
+Invoke-WebRequest -Method 'DELETE' -Uri $Url -ContentType "application/json"
+---draft scripts:
+
+
+curl -X PUT -H "Content-Type: application/json" -d '{"artist": "Sonic Youth", "title": "Silver Rocket", "last_play": "2017-10-18 15:15:26", "duration": 200}' http://localhost:8000/tracks/1
 
 $Request = Invoke-WebRequest -Uri $Uri -Method Post -Form $Form
 echo $Request
+
+$Url = 'http://127.0.0.1:8000/tracks/'
+$Form = '{"artist": "Sonic Youth", "title": "Silver Rocket", "last_play": "2017-10-18 15:15:26", "duration": 200}'
+Invoke-WebRequest -Method 'Post' -Uri $Url -Body $Form -ContentType "application/json"
+
+
+
+
+Invoke-WebRequest -Method 'Post' -Uri $url -Body ({"artist"= "Sonic Youth"}|ConvertTo-Json) -ContentType "application/json"
+
+curl -X POST -H "Content-Type: application/json" -d '{"artist": "Sonic Youth", "title": "Silver Rocket", "last_play": "2017-10-18 15:15:26", "duration": 200}' http://localhost:8000/tracks
